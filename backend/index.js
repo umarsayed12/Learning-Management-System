9c0e3a2b04c58f1deef168118ee0a3f2da7e5b3c
Umar Khursheed <umarkhursheed979@gmail.com>
2025-04-26 01:41:10 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
 
+dotenv.config({});
 const app = express();
-const PORT = 8000;
 
 app.get("/", (req, res) => {
-  res.send("Hello Umar!");
+  res.send("Hello Bachhon!!!");
 });
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
