6e33b30a5b2f5b22af515ed6fc8fc9a4a768431e
Umar Khursheed <umarkhursheed979@gmail.com>
2025-04-27 15:25:14 +0530

    Written Business Login for User Register and Login

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,63 @@
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../utils/generateToken.js";
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password)
+      return res.status(400).json({
+        success: false,
+        message: "All Fields are Required.",
+      });
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({
+        success: false,
+        message: "User with this email already exists.",
+      });
+    }
+    const hashedPass = await bcrypt.hash(password, 10);
+    await User.create({
+      name,
+      email,
+      password: hashedPass,
+    });
+    return res.status(201).json({
+      success: true,
+      message: "Account created successfully.",
+    });
+  } catch (error) {
+    console.log("Registration Error : ", error);
+    return res.status(500).json({
+      success: false,
+      message: "Error : Failed to Register",
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!name || !email || !password)
+      return res.status(400).json({
+        success: false,
+        message: "All Fields are Required.",
+      });
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid Credentials. Please try again!",
+      });
+    }
+    const isPassMatch = await bcrypt.compare(password, user.password);
+    if (!isPassMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid Credentials. Please try again!",
+      });
+    }
+    generateToken(res, user, `Welcome Back ${user.name}`);
+  } catch (error) {}
+};
